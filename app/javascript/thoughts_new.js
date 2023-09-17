5cf796384b026872a45fb0a0c6551ca8880f794b
Xenrathe <126534032+Xenrathe@users.noreply.github.com>
2023-09-16 22:38:24 -0400

    Fix javascript bug related to form validation error in thoughts new view

--- app/javascript/thoughts_new.js
+++ app/javascript/thoughts_new.js
@@ -1,12 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const textarea = document.querySelector('.thoughts-new .content-input');
+
+
+function updateCharacterCount()
+{
+  const textarea = document.getElementById('thought_content');
   const characterCount = document.getElementById('content-char-count');
+  const startingCharacters = textarea.getAttribute('maxlength') - textarea.value.length;
+  characterCount.textContent =  startingCharacters + " characters remaining";
 
   textarea.addEventListener('input', function() {
-    const currentLength = textarea.value.length;
-    const maxLength = textarea.getAttribute('maxlength');
-    const remainingCharacters = maxLength - currentLength;
+  const currentLength = textarea.value.length;
+  const maxLength = textarea.getAttribute('maxlength');
+  const remainingCharacters = maxLength - currentLength;
 
-    characterCount.textContent = remainingCharacters + " characters remaining";
+  characterCount.textContent = remainingCharacters + " characters remaining";
   });
-});
+}
+
+updateCharacterCount();
+document.addEventListener('turbo:render', function() { updateCharacterCount(); });
